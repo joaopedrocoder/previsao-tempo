c16ac3d4f11b624750049aa24a28d76747328ec1
Joao Pedro <joaopedrolr.7@gmail.com>
2024-03-19 10:40:42 -0300

    criação dos componentes de home e bookmarks

--- src/app/pages/bookmarks/bookmarks.module.ts
+++ src/app/pages/bookmarks/bookmarks.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BookmarksRoutingModule } from './bookmarks-routing.module';
+import { BookmarksComponent } from './bookmarks.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BookmarksComponent
+  ],
   imports: [
     CommonModule,
     BookmarksRoutingModule
